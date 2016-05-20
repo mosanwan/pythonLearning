08dccb209043bd430e199205208fa41c8c1dabe0
Sanwu-three <xtygah14three@hotmail.com>
2016-05-20 10:00:04 +0800

    测试提交

--- HelloPython.py
+++ HelloPython.py
@@ -1,19 +1,3 @@
-"""
-What that
-"""
 
-# print("Hello World")
-# print("Next")
-
-def mytestfunction():
-    "测试函数"
-    _my_var = 100
-    print("Hello " + str(_my_var))
-
-mytestfunction()
-MYARRAY = [10, 20, 30, 40]
-MYARRAY[0] = 100
-print(MYARRAY)
-MY_VAR = 100
 
 
